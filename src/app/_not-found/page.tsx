345696157250bca1e0de3f2d05f4b109e85c7d22
mohitbharari10 <mohitwebglaze10@gmail.com>
2025-11-25 14:46:11 +0530

    new test

---
+++ src/app/_not-found/page.tsx
@@ -0,0 +1,11 @@
+// src/app/_not-found/page.tsx
+export const runtime = "edge";
+
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
