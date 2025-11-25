48f79d489e522aa7e428aa8f36b543e88e7098ba
mohitbharari10 <mohitwebglaze10@gmail.com>
2025-11-25 15:21:58 +0530

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+      <p>The page you are looking fodr does not exist.</p>
     </div>
   );
 }
